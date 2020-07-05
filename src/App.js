5e1de44ed862c600577d0f10b3ad14f12e3415e6
Salman Munawar <samcisian@gmail.com>
2020-07-05 17:04:38 +0500

    Made some changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p>       
+        <div>
+          Updated by Salman
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
